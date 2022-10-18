0998eb61452aa1b8e44520142270df9457ad2ba0
Nywgates <aballea.louis@gmail.com>
2022-10-18 08:50:49 +0200

    ex03 done

---
+++ Module 02/ex03/gradient.py
@@ -0,0 +1,48 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+def add_intercept(x):
+    if (not isinstance(x, np.ndarray)):
+        return None
+    if (len(x.shape) == 1):
+        x = np.reshape(x, (x.shape[0], 1))
+    shape = x.shape
+    x = np.insert(x, 0, np.ones(x.shape[0]), axis=1)
+    return np.reshape(x, (shape[0], shape[1] + 1))
+
+def predict_(x, theta):
+    if (not isinstance(x, np.ndarray) or not isinstance(theta, np.ndarray)):
+        return None
+    if (x.size == 0 or theta.size == 0):
+        return None
+    if (len(theta) != x.shape[1] + 1):
+        return None
+    x = add_intercept(x)
+    return x.dot(theta)
+
+def gradient(x, y, theta):
+    """Computes a gradient vector from three non-empty numpy.array, without any for-loop.
+    The three arrays must have the compatible dimensions.
+    Args:
+    x: has to be an numpy.array, a matrix of dimension m * n.
+    y: has to be an numpy.array, a vector of dimension m * 1.
+    theta: has to be an numpy.array, a vector (n +1) * 1.
+    Return:
+    The gradient as a numpy.array, a vector of dimensions n * 1,
+    containg the result of the formula for all j.
+    None if x, y, or theta are empty numpy.array.
+    None if x, y and theta do not have compatible dimensions.
+    None if x, y or theta is not of expected type.
+    Raises:
+    This function should not raise any Exception.
+    """
+    if (not isinstance(y, np.ndarray) or not isinstance(x, np.ndarray) or not isinstance(theta, np.ndarray)):
+        print("Invalid type.")
+        return None
+    if (len(y) != len(x) or theta.shape[0] != x.shape[1] + 1):
+        print("Invalid shape.")
+        return None
+    fct = 1 / len(x)
+    x_hat = predict_(x, theta)
+    x = add_intercept(x).T
+    return np.array(fct * (x.dot((x_hat - y))))
